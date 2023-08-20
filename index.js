3e4a12acb94d1b49890b8ef87332bad60a250641
okshi <johnstarhafiz@gmail.com>
2023-08-20 11:24:16 +0600

    Create GET API for load default all data from database services collection

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 const uri = `mongodb://${process.env.SECRET_USER_NAME}:${process.env.SECRET_KEY_PASSWORD}@cluster0-shard-00-00.luy9u.mongodb.net:27017,cluster0-shard-00-01.luy9u.mongodb.net:27017,cluster0-shard-00-02.luy9u.mongodb.net:27017/?ssl=true&replicaSet=atlas-9im02q-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Create a MongoClient
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -28,12 +28,12 @@ async function run() {
     //collection and database
     const serviceCollection = client.db("carDB").collection("services");
 
-    // //get method (default all)
-    // app.get("/services", async (req, res) => {
-    //   const cursor = serviceCollection.find();
-    //   const result = await cursor.toArray();
-    //   res.send(result);
-    // });
+    //get method (default all)
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     //Ping to successful connection
     await client.db("admin").command({ ping: 1 });
