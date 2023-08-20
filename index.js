096a258a6cc1636b913ef18157f700341af60ec7
okshi <johnstarhafiz@gmail.com>
2023-08-20 11:21:47 +0600

    Change database uri version for connection problem

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.SECRET_USER_NAME}:${process.env.SECRET_KEY_PASSWORD}@cluster0.luy9u.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb://${process.env.SECRET_USER_NAME}:${process.env.SECRET_KEY_PASSWORD}@cluster0-shard-00-00.luy9u.mongodb.net:27017,cluster0-shard-00-01.luy9u.mongodb.net:27017,cluster0-shard-00-02.luy9u.mongodb.net:27017/?ssl=true&replicaSet=atlas-9im02q-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,7 +26,14 @@ async function run() {
     await client.connect();
 
     //collection and database
-    const serveceCollection = client.db("carDB").collection("services");
+    const serviceCollection = client.db("carDB").collection("services");
+
+    // //get method (default all)
+    // app.get("/services", async (req, res) => {
+    //   const cursor = serviceCollection.find();
+    //   const result = await cursor.toArray();
+    //   res.send(result);
+    // });
 
     //Ping to successful connection
     await client.db("admin").command({ ping: 1 });
