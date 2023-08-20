c3e27591d83d6303258918c0c8c8e0131211f50b
okshi <johnstarhafiz@gmail.com>
2023-08-20 11:50:11 +0600

    load single service data by its specific data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -32,6 +32,14 @@ async function run() {
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //get method (specific one by id)
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
       res.send(result);
     });
 
