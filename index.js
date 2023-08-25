c5868b6654656a8d5b333543a4e12471794a4c70
okshi <johnstarhafiz@gmail.com>
2023-08-25 19:37:22 +0600

    Get user email when user loggedin our system and post it jwt route

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ async function run() {
 
     app.post("/jwt", (req, res) => {
       const user = req.body;
+      console.log(user);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "1h",
       });
