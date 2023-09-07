2a0cea0b474ab3665252e6a81a7ff98d02c09a29
okshi <johnstarhafiz@gmail.com>
2023-09-07 23:17:01 +0600

    Refector jwt api

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ async function run() {
     //JWT Route for Token Creation
     app.post("/jwt", (req, res) => {
       const user = req.body;
-      console.log(user);
+      // console.log(user);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "1h",
       });
